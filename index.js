82adc7ca42f6bf110795fffa0ff18b37c56c2b0e
TPL822_6 <tpl822_6@TPL822-6s-MBP.attlocal.net>
2024-08-26 16:28:18 -0700

    project so far

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-import express from 'express';
-import BOOKS from './books.js'
-import { Router } from 'express';
-
+import express from "express";
+import bodyParser from "body-parser";
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const app = express();
-const route = express.Router();
-const books= BOOKS;
+const PORT = 3000;
+app.use(bodyParser.json());
 
-app.get('/books', (red, res) =>{
-    req.json()
+
+const pool = new Pool({
+    user: "tpl822_6",
+    host:"localhost",
+    database: "tpl822_6", 
+    password:"",
+    port:5432,
+});
+
+app.get('/actor', async(req, res) =>{  
+    const data =  await pool.query('SELECT * FROM actor')
+   res.json(data.rows);
+});
+
+app.get('/actor/:id', async(req, res) =>{  
+    const{id} =  req.params;
+    const dt =  await pool.query('SELECT * FROM actor WHERE id = $1', [id])
+   res.json(dt.rows[0]);
+});
+
+//app.post();
+
+// app.put();
+
+
+app.delete('/actor/:id', async(req, res) =>{  
+    const{id} =  req.params;
+    const dt =  await pool.query('SELECT * FROM actor WHERE id = $1', [id])
+   res.json(dt.rows[0]);
 });
 
 
-app.post();
-
-app.put();
-
-
-app.delete();
 
 
 
-
-
-app.listen(3000);
+app.listen(PORT , () => console.log(`server start on: ${PORT}`));
